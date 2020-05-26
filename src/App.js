524dcaac026660cd65f3b9f65304c66e8e017c44
mafergus <matthew.n.ferguson@gmail.com>
2020-05-26 20:39:30 +0400

    First

--- src/App.js
+++ src/App.js
@@ -2,23 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const recipes = [
+  {
+    author: "Jim",
+    name: "Chicken Curry",
+    description: "Delicious spicy chicken curry"
+  },
+  {
+    author: "Aravind",
+    name: "Hamburger",
+    description: "Juicy burger with toppings and a soft bun",
+  }
+]
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
